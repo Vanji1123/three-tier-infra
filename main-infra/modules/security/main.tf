41aa2539bcf99a518ba2a1b81651cbd245e3d73f
Vanji <vanjimuthus@gmail.com>
2026-05-22 22:41:34 +0530

    fixed permanent rds sg connectivity

--- main-infra/modules/security/main.tf
+++ main-infra/modules/security/main.tf
@@ -1,60 +1,3 @@
-resource "aws_security_group" "eks_sg" {
-
-  name = "${var.project_name}-eks-sg"
-
-  description = "EKS Cluster Security Group"
-
-  vpc_id = var.vpc_id
-
-  ingress {
-
-    from_port = 0
-
-    to_port = 0
-
-    protocol = "-1"
-
-    self = true
-  }
-
-  ingress {
-
-    from_port = 80
-
-    to_port = 80
-
-    protocol = "tcp"
-
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-
-    from_port = 443
-
-    to_port = 443
-
-    protocol = "tcp"
-
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-
-    from_port = 0
-
-    to_port = 0
-
-    protocol = "-1"
-
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "${var.project_name}-eks-sg"
-  }
-}
-
 resource "aws_security_group" "rds_sg" {
 
   name = "${var.project_name}-rds-sg"
@@ -71,7 +14,9 @@ resource "aws_security_group" "rds_sg" {
 
     protocol = "tcp"
 
-    security_groups = [aws_security_group.eks_sg.id]
+    security_groups = [
+      aws_eks_cluster.main_cluster.vpc_config[0].cluster_security_group_id
+    ]
   }
 
   egress {
